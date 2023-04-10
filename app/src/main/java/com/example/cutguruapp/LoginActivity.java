19f90ed93bfff0523527e5ed53640a7451cae6e9
Katy Reshetnyk <kreshetnyk@gmail.com>
2023-04-10 19:56:14 +0300

    first commit

---
+++ app/src/main/java/com/example/cutguruapp/LoginActivity.java
@@ -0,0 +1,67 @@
+package com.example.cutguruapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText email_login;
+    private EditText password_login;
+    private Button exit_login;
+    private TextView register_txt;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        email_login = findViewById(R.id.email_login);
+        password_login = findViewById(R.id.password_login);
+        exit_login = findViewById(R.id.exit_login);
+        register_txt = findViewById(R.id.register_txt);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        register_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        exit_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (email_login.getText().toString().isEmpty() || password_login.getText().toString().isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Поля не можуть бути порожніми.", Toast.LENGTH_LONG).show();
+                } else {
+                    mAuth.signInWithEmailAndPassword(email_login.getText().toString(), password_login.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "Є помилка.", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+
+                            });
+                    }
+                }
+             });
+        }
+}
---
+++ app/src/main/java/com/example/cutguruapp/RegisterActivity.java
@@ -0,0 +1,60 @@
+package com.example.cutguruapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText name_register;
+    private EditText email_register;
+    private EditText password_register;
+    private Button exit_register;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mAuth=FirebaseAuth.getInstance();
+        name_register= findViewById(R.id.name_register);
+        email_register= findViewById(R.id.email_register);
+        password_register= findViewById(R.id.password_register);
+        exit_register= findViewById(R.id. exit_register);
+        exit_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(name_register.getText().toString().isEmpty()|| email_register.getText().toString().isEmpty()|| password_register.getText().toString().isEmpty()) {
+                    Toast.makeText(RegisterActivity.this,"Поля не можуть бути пoрожніми.", Toast.LENGTH_LONG).show();
+                }else {
+                    mAuth.createUserWithEmailAndPassword(email_register.getText().toString(),password_register.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        Intent intent= new Intent(RegisterActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                    }else{
+                                        Toast.makeText(RegisterActivity.this,"Не правельно введений пароль або логін\n"
+                                                +"Спробуйте ще раз",Toast.LENGTH_LONG);
+                                    }
+
+                                }
+                            });
+
+                }
+            }
+        });
+
+    }
+    }
